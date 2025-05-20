78a4be4ee8fd8cd5eadd29d3b07780d58ae0b02b
Jacqueb-1337 <61178380+Jacqueb-1337@users.noreply.github.com>
2025-05-20 17:53:56 -0500

    Add files via upload

--- app/src/main/python/nsz_script.py
+++ app/src/main/python/nsz_script.py
@@ -1,1 +1,12 @@
+import nsz
+import os
+import tempfile
 
+def decompress(input_stream, output_folder_uri):
+    temp_path = tempfile.NamedTemporaryFile(delete=False).name
+    with open(temp_path, "wb") as f:
+        f.write(input_stream.read())
+
+    output_path = os.path.join("/storage/emulated/0/Download/NSZ_Converted/", os.path.basename(temp_path).replace(".nsz", ".nsp"))
+    nsz.decompress(temp_path, output_path)
+    return output_path
