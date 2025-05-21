0a3602ae84c423cc342f94f46c40966ba3bbd6a3
Jacqueb-1337 <61178380+Jacqueb-1337@users.noreply.github.com>
2025-05-21 17:20:51 -0500

    Update main.py

--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -1,8 +1,33 @@
 # app/src/main/python/main.py
 
-import builtins
-builtins.input = lambda *args, **kwargs: None  # Disable any call to input()
+import sys
+import types
+import warnings
 
+# Disable input() to avoid EOFError on Android
+__builtins__.input = lambda *args, **kwargs: None
+
+# Dummy curses module to prevent crash on Android
+sys.modules["curses"] = types.SimpleNamespace(
+    initscr=lambda: None,
+    endwin=lambda: None,
+    wrapper=lambda func, *args, **kwargs: func(*args, **kwargs),
+    newwin=lambda *a, **kw: None,
+    noecho=lambda: None,
+    cbreak=lambda: None,
+    echo=lambda: None,
+    nocbreak=lambda: None,
+    curs_set=lambda x: None,
+    start_color=lambda: None,
+    init_pair=lambda a, b, c: None,
+    color_pair=lambda x: 0,
+    has_colors=lambda: False,
+)
+
+# Suppress DeprecationWarnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+# NSZ main
 import nsz
 
 def convert_nsz_to_nsp(input_file, output_dir):
