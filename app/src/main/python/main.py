3c2cb5024521894653f44b74b20134e1f2508a27
Jacqueb-1337 <61178380+Jacqueb-1337@users.noreply.github.com>
2025-05-21 15:29:13 -0500

    Update main.py

--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -1,12 +1,4 @@
-import nsz
-import sys
+import nsz.__main__
 
-def convert_nsz_to_nsp(input_path, output_dir):
-    sys.argv = [
-        "nsz",
-        input_path,
-        "--outdir", output_dir,
-        "--verify",
-        "--overwrite"
-    ]
-    nsz.main()
+def main(argv):
+    return nsz.__main__.main(argv)
